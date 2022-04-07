ee6944064e901eddadee65ad892ac65f97bb4909
Swann <swann.bouviermuller@gmail.com>
2022-04-07 12:11:19 +0200

    update slug label

--- django_docx_template/models.py
+++ django_docx_template/models.py
@@ -23,7 +23,7 @@ def upload_to_hook(instance: "DocxTemplate", filename: str) -> str:
 
 
 class DocxTemplate(models.Model):
-    slug = models.SlugField("slug", primary_key=True, blank=True)
+    slug = models.SlugField("Slug", primary_key=True, blank=True)
     name = models.CharField("Name", max_length=100)
     # description = models.TextField("Description", blank=True, null=True)
     docx = models.FileField(upload_to=upload_to_hook)
